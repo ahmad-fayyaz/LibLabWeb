332814be4dc75535bdc922240b2dff4e78b5e370
afayyaz1 <afayyaz1@swarthmore.edu>
2024-07-01 13:44:49 -0400

    init commit

--- liblab/documentation/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.get_sidebar_data, name='sidebar'),
-    path('document/<slug:slug>/', views.document_detail, name='document_detail'),
-]
--- liblabWeb/liblabWeb/wsgi.py
+++ liblabWeb/liblabWeb/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for liblab project.
+WSGI config for liblabWeb project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'liblab.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'liblabWeb.settings')
 
 application = get_wsgi_application()
